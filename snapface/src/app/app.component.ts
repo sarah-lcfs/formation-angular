72397dae96ab9c9d3483a59e6e6e297da2be116a
sarah-lcfs <s.lacoffrette@cat-amania.com>
2024-12-23 15:08:03 +0100

    add :: array

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -11,15 +11,21 @@ import { FaceSnap } from './models/face-snap';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+
+  faceSnaps!: FaceSnap[];
+
   mySnap!: FaceSnap;
   myOtherSnap!: FaceSnap;
   myLastSnap!: FaceSnap;
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap("First", "ma description",  "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg", new Date(), 5 );
-    this.myOtherSnap = new FaceSnap("Other", "ma description",  "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg", new Date(), 5 );
-    this.myLastSnap = new FaceSnap("Last", "ma description",  "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg", new Date(), 5 );
-    this.myOtherSnap.setLocation("à la montagne");
+    this.faceSnaps = [
+    new FaceSnap("First", "ma description",  "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg", new Date(), 5 ),
+    new FaceSnap("Other", "ma description",  "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg", new Date(), 5 ),
+    new FaceSnap("Last", "ma description",  "https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg", new Date(), 5 )
+    ]
+    
+    this.faceSnaps[1].setLocation("à la montagne");
   }
 
 }
