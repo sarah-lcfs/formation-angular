440b65d8be8d2a2365d3336058c0cf32ad0a1c4a
sarah-lcfs <s.lacoffrette@cat-amania.com>
2024-12-23 16:20:30 +0100

    add :: uppercase

--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
-import { NgStyle, NgClass } from '@angular/common';
+import { NgStyle, NgClass,UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap',
   standalone: true,
   imports: [
     NgStyle, 
-    NgClass
+    NgClass,
+    UpperCasePipe
   ],
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
