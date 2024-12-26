286248bfe3a0405004b692f38f692f9c716d988f
sarah-lcfs <s.lacoffrette@cat-amania.com>
2024-12-26 11:54:30 +0100

    add :: router link (nav and event)

--- snapface/src/app/landing-page/landing-page.component.ts
+++ snapface/src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
   imagePath = './assets/images/logo.png';
+  constructor(private router: Router){}
+
+  onContinue() {
+    this.router.navigateByUrl('facesnaps');
+  }
 }
